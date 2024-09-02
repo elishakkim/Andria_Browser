1594b18206f8108cde1e4f9416472fb09ba40adb
Elisha <elishakimcoding@gmail.com>
2024-09-01 20:41:03 -0400

    Logs

---
+++ build/Qt_6_7_2_for_macOS-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Andria_FILE /Users/elishakim/Documents/coding/Andria/build/Qt_6_7_2_for_macOS-Release/Andria.app/Contents/MacOS/Andria)
+set(__QT_DEPLOY_TARGET_Andria_TYPE EXECUTABLE)
---
+++ build/Qt_6_7_2_for_macOS-Release/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/elishakim/Qt/6.7.2/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/elishakim/Documents/coding/Andria/build/Qt_6_7_2_for_macOS-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/elishakim/Qt/6.7.2/macos" CACHE "PATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
